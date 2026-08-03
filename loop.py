b203a7cc3a2cfd9fe6f2d39c64e500c77eadd99e
roheet032 <roheet032>
2026-08-03 19:32:54 +0545

    new learn

---
+++ loop.py
@@ -0,0 +1,21 @@
+
+
+
+for i in range(3,51):
+    if(i==21):
+        break
+    if (i%3==0):
+        print(i)
+        
+
+print('out of loop')
+
+
+#list
+marks=[90,91,92,93]
+
+print(marks[:2])
+
+
+for score in marks:
+    print(marks)
